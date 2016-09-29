b6af2776c711734b1182065f2b64b48a046fcc94
Paul Irish <paul.irish@gmail.com>
2016-09-28 18:37:32 -0700

    mostly works. test via ▸ tsc -w -p ./jsconfig.json

---
+++ lighthouse-cli/typings/index.d.ts
@@ -0,0 +1,9 @@
+/// <reference path="globals/node/index.d.ts" />
+
+/// <reference path="../decl/ask.d.ts" />
+/// <reference path="../decl/chrome-finder.d.ts" />
+/// <reference path="../decl/chrome-launcher.d.ts" />
+/// <reference path="../decl/index.d.ts" />
+/// <reference path="../decl/printer.d.ts" />
+
+
